3a8a2abc41c20dc944906718d172bdb69f24ef2d
dujin kim <kdj00718@gmail.com>
2020-11-11 13:34:46 -0800

    finished menu.html

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,35 @@
 // Google Mpas
-
 function initMap() {
   // Map options
-var options = {
-    zoom:15,
-    center:{lat:49.249914, lng:-123.074965 }
+  var options = {
+    zoom: 15,
+    center: {
+      lat: 49.249914,
+      lng: -123.074965
+    }
+  }
+
+  //new map
+  var map = new google.maps.Map(document.getElementById("map"), options);
+
+  // Add marker
+  var marker = new google.maps.Marker({
+    position: {
+      lat: 49.249914,
+      lng: -123.074965
+    },
+    map: map
+  });
 }
 
-//new map
-var map = new google.maps.Map(document.getElementById("map"), options);
 
-// Add marker
-var marker = new google.maps.Marker({
-  position:{lat:49.249914,lng: -123.074965},
-  map:map
-});
+// Read gym name and set website and phone number.
+function readMainInfo() {
+  db.collection("gyms").doc("01").onSnapshot(function (snap) {
+    // console.log(snap.data().name);
+    document.getElementById("gym-name").innerText = snap.data().name;
+    document.getElementById("gym-link").href = snap.data().website;
+    document.getElementById("phone").href = snap.data().phone;
+  })
 }
-
+readMainInfo();
