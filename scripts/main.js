3990b8b0673b8901f332448720603071e7ba3fec
dujin kim <kdj00718@gmail.com>
2020-11-17 19:15:20 -0800

    main.css + firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
-
-// Read gym info
-function readMainInfo() {
-  db.collection("gyms").doc("01").onSnapshot(function (snap) {
-    // console.log(snap.data().name);
-    document.getElementById("gym-name0").innerText = snap.data().name;
+function getGyms(){
+  db.collection("gyms")
+  .get()
+  .then(function(snap){
+    snap.forEach(function (doc) {
+      var name = doc.data().name;
+      var rating = doc.data().ratings[0];
+      console.log(name);
+      var cont = document.getElementById("ww");
+      cont.innerHTML += "<a href='menu.html' class='panel'></a>";
+      cont.innerHTML += "<div class='item'><img src='../images/full-logo.png' alt='gym picture'/><div class='rating'><h3 class='gym-name'>" + name + "</h3><p>Overall Rating</p><span class='overall-rating'><i class='fas fa-star'><i class='fas fa-star-half-alt'></i><i class='far fa-star'></i></span></div>";
+    })
   })
 }
-readMainInfo();
+getGyms();
--- scripts/maindemo.js
+++
@@ -1,7 +0,0 @@
-function getGyms(){
-  db.collection("gyms")
-  .get()
-  .then(function(snap){
-    snap.forEach
-  })
-}
