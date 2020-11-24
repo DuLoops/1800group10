2da7ce452afffe49c5288f9e42208db5bfcfba79
dujin kim <kdj00718@gmail.com>
2020-11-24 09:56:10 -0800

    main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,9 @@ function getGyms(){
     snap.forEach(function (doc) {
       var name = doc.data().name;
       var rating = doc.data().ratings[0];
-      console.log(name);
-      var cont = document.getElementById("ww");
-      cont.innerHTML += "<a href='menu.html' class='panel'></a>";
-      cont.innerHTML += "<div class='item'><img src='../images/full-logo.png' alt='gym picture'/><div class='rating'><h3 class='gym-name'>" + name + "</h3><p>Overall Rating</p><span class='overall-rating'><i class='fas fa-star'><i class='fas fa-star-half-alt'></i><i class='far fa-star'></i></span></div>";
+      console.log("loading gym: " + name);
+      var cont = document.getElementById("container");
+      cont.innerHTML += "<a href='menu.html' class='panel'><div class='item'><img src='../images/full-logo.png' alt='gym picture'/><div class='rating'><h3 class='gym-name'>" + name + "</h3><p>Overall Rating</p><span class='overall-rating'><i class='fas fa-star'><i class='fas fa-star-half-alt'></i><i class='far fa-star'></i></span></div></a>";
     })
   })
 }
