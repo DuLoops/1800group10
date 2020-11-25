535ea41ec88bba5a380c656da35d44880e917d46
Abell Rede <arede.business@gmail.com>
2020-11-25 02:51:19 -0800

    Updating html pages

--- scripts/review.js
+++ scripts/review.js
@@ -1,9 +1,13 @@
 //* Reads the title of the review as well as the content of the review
 function readMainInfo() {
-  db.collection("gyms").doc("01").collection("reviews").doc("01").onSnapshot(function(snap) {
-    //console.log(snap.data().title);
-    document.getElementById("review1").innerText = snap.data().review;
-    document.getElementById("review-title1").innerText = snap.data().title;
-  })
+	db.collection("gyms")
+		.doc("01")
+		.collection("reviews")
+		.doc("01")
+		.onSnapshot(function (snap) {
+			//console.log(snap.data().title);
+			document.getElementById("review1").innerText = snap.data().review;
+			document.getElementById("review-title1").innerText = snap.data().title;
+		});
 }
 readMainInfo();
