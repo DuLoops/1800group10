adf3d180adefda42acf027c1c93290660b1a6198
Abell Rede <arede.business@gmail.com>
2020-11-29 10:39:00 -0800

    Linking html pages to firebase

---
+++ scripts/write_review.js
@@ -0,0 +1,23 @@
+const submitBtn = document.querySelector("#submit");
+
+let reviewTitle = document.querySelector("#reviewTitle");
+let reviewContent = document.querySelector("#reviewContent");
+
+submitBtn.addEventListener("click", function () {
+	let reviewTitleInput = reviewTitle.value;
+	let reviewContentInput = reviewContent.value;
+
+	//** Access the Database Collection */
+	db.collection("review")
+		.doc("review1")
+		.set({
+			Title: reviewTitleInput,
+			Review: reviewContentInput,
+		})
+		.then(function () {
+			console.log("Data Saved");
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+});
